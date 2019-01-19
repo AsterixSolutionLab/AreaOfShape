80e925cb2ee56a1f7aac5593bd4c7dd94f19dc07
ranjanb91 <rashmi.behera91@gmail.com>
2019-01-19 09:23:34 +0530

    Added Area of Rectangle

---
+++ AreaOfShape.java
@@ -0,0 +1,12 @@
+public class AreaOfShape{
+
+	public static void areaOfRectangle(float length, float width){
+		System.out.println("Area of Rectangle is:" + (length * width));
+	}
+	
+	public static void main(String[] args){
+		areaOfRectangle(3.59f, 4.545f);
+ 
+	}
+
+}
